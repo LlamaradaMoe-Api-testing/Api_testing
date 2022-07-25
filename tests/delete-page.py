66d08e896b6d9891b3930130394dec29f8403004
Amilcar-B <Amilcar.Barriento@fundacion-jala.org>
2022-07-25 11:52:01 -0300

    new branch to started from develop

---
+++ tests/delete-page.py
@@ -0,0 +1,21 @@
+from assertpy.assertpy import assert_that
+from utils.config import BASE_URI, AUTHORIZATION
+from utils.requests import Requests
+from helpers.print_helpers import pretty_print
+
+
+def test_delete():
+    status_code = 1
+    dict_response = 0
+    id = "8"
+    payload = {}
+    headers = {
+        'Authorization': AUTHORIZATION,
+        'Content-Type': 'application/json'
+    }
+
+    response = Requests(f'{BASE_URI}/wp/v2/pages/{id}', headers, payload)
+    responses = response.get_responses(response.get_request('delete'))
+    assert_that(responses[status_code]).is_equal_to(200)
+    pretty_print(responses[dict_response])
+test_delete()
---
+++ utils/config.py
@@ -0,0 +1,4 @@
+BASE_URI = "http://localhost/wordpress/wp-json"
+USERNAME = "abarriento"
+PASSWORD = "124ares3547"
+AUTHORIZATION = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsIm5hbWUiOiJhYmFycmllbnRvIiwiaWF0IjoxNjU4NzU5MzI4LCJleHAiOjE4MTY0MzkzMjh9.z1r1BGGjp39Bkrs3DePTV9w_7Nla16Ox7tMTPw05rAg"
