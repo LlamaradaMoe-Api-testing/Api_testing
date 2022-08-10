2a70be21f38e344572a59098ccfb99ce1dfcd458
Freddy Claros <Freddy.Claros@fundacion-jala.org>
2022-08-09 23:50:37 -0400

    Added setup and teardown for get pages

--- tests/test_get_page.py
+++ tests/test_get_page.py
@@ -32,6 +32,21 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
+@allure.title('Setting the environment')
+@allure.step('Preparing request')
+@pytest.fixture(autouse=True)
+def setup_requisites():
+    get_token()
+    global requests
+    requests = CrudPage().post(body())
+    global title
+    title = requests[dict_response]['title']['rendered']
+    id_page = requests[dict_response]['id']
+    yield
+    payload = {}
+    CrudPage().delete(id_page, payload)
+
+
 # Happy path
 @allure.severity(allure.severity_level.CRITICAL)
 @pytest.mark.acceptance
@@ -42,7 +57,6 @@ logger.setLevel(logging.DEBUG)
 @allure.step("Method: test_get_all")
 def test_get_all():
     logger.info('Executed test for validate get all pages correctly')
-    get_token()
     responses = CrudPage().get_all()
     assert_that(responses[status_code]).is_equal_to(HTTPStatus.OK)
     pretty_print(responses[json_response])
@@ -59,9 +73,6 @@ def test_get_all():
 @allure.step("Method: test_get_page_id_publish")
 def test_get_page_id_publish():
     logger.info('Execute test for get a page by id')
-    get_token()
-    requests = CrudPage().post(body())
-    title = requests[dict_response]['title']['rendered']
     responses = CrudPage().get_by_id(requests[dict_response]['id'])
     json = responses[json_response]
     dict = responses[dict_response]
@@ -82,9 +93,6 @@ def test_get_page_id_publish():
 @allure.step("Method: test_get_page_id_trash")
 def test_get_page_id_trash():
     logger.info('Execute test for get a page created with status "trash"')
-    get_token()
-    requests = CrudPage().post(body())
-    title = requests[dict_response]['title']['rendered']
     payload = {}
     responses = CrudPage().delete(requests[dict_response]['id'], payload)
     assert_that(responses[status_code]).is_equal_to(HTTPStatus.OK)
@@ -108,7 +116,6 @@ def test_get_page_id_trash():
 @allure.step("Method: test_get_page_id_draft")
 def test_get_page_id_draft():
     logger.info('Execute test for get a page created with status "draft"')
-    get_token()
     requests = CrudPage().post(schema_draft())
     title = requests[dict_response]['title']['rendered']
     responses = CrudPage().get_by_id(requests[dict_response]['id'])
@@ -120,6 +127,9 @@ def test_get_page_id_draft():
     pretty_print(json)
     logger.debug(f'Request executed successfully')
     logger.info('Test for get a page created with status "draft" executed successfully')
+    id = requests[dict_response]['id']
+    payload = {}
+    CrudPage().delete(id, payload)
 
 
 # Negative Test
@@ -131,7 +141,6 @@ def test_get_page_id_draft():
 @allure.step("Method: test_get_id_notexist")
 def test_get_id_notexist():
     logger.info('Execute test for validate status not found (404) at get a page with no existing id')
-    get_token()
     id = 'cuare'
     responses = CrudPage().get_by_id(id)
     assert_that(responses[status_code]).is_equal_to(HTTPStatus.NOT_FOUND)
@@ -148,18 +157,20 @@ def test_get_id_notexist():
 @allure.step("Method: test_get_page_id_is_not_publish")
 def test_get_page_id_is_not_publish():
     logger.info('Execute test for validate if page created with an invalid id is not displayed')
-    get_token()
     requests = CrudPage().post(schema_draft())
     title = requests[dict_response]['title']['rendered']
     responses = CrudPage().get_by_id(requests[dict_response]['id'])
     json = responses[json_response]
-    dict = responses[dict_response]
+    dicto = responses[dict_response]
     assert_that(responses[status_code]).is_equal_to(HTTPStatus.OK)
-    assert_that(dict['title']['rendered']).is_equal_to(title)
-    assert_that(dict['status']).is_not_equal_to('publish')
+    assert_that(dicto['title']['rendered']).is_equal_to(title)
+    assert_that(dicto['status']).is_not_equal_to('publish')
     pretty_print(json)
     logger.debug(f'Request executed successfully')
     logger.info('Test for validate if page created with an invalid id is not displayed executed successfully')
+    id = requests[dict_response]['id']
+    payload = {}
+    CrudPage().delete(id, payload)
 
 
 @pytest.mark.negative
@@ -167,12 +178,9 @@ def test_get_page_id_is_not_publish():
 @allure.suite("regression")
 @allure.suite("negative")
 @allure.title("test for validate the status of page is not trash")
-@allure.step("Method: test_get_page_id_is_not_publish")
-def test_get_page_id_is_not_publish():
+@allure.step("Method: test_get_page_id_is_not_trash")
+def test_get_page_id_is_not_trash():
     logger.info('Executed test for get pages with validate the status not trash')
-    get_token()
-    requests = CrudPage().post(body())
-    title = requests[dict_response]['title']['rendered']
     responses = CrudPage().get_by_id(requests[dict_response]['id'])
     json = responses[json_response]
     dict = responses[dict_response]
